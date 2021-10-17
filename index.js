59128c42d63eafafabbac6feef7c109aa800734e
Becky Hassler <beckyjhassler@gmail.com>
2021-10-17 17:13:46 -0400

    Add event listner to add game functionality

--- index.js
+++ index.js
@@ -15,16 +15,41 @@ var winsEl = document.getElementById('wins')
 var lossesEl = document.getElementById('losses')
 
 // target all .box elements and attach a click event listener to each one using a loop
+var boxElements = document.querySelectorAll('.box')
 
-// within each click event...
-// determine which box was clicked with 'this.textContent' or event.target.textContent
-// convert that value to a Number and store it to a variable
+for (var i=0; i < boxElements.length; i++) {
+    var boxElement = boxElements[i];
 
-// create a random number between 1-3 and store it to a variable
-// This number will represent the winning box
+    // within each click event...
+    // determine which box was clicked with 'this.textContent' or event.target.textContent
+    // convert that value to a Number and store it to a variable
+    boxElement.addEventListener("click", function(event) {
+        var boxNumWasClicked = event.target.textContent
+        var numWasClicked = (parseInt(boxNumWasClicked, 10))
+        console.log(numWasClicked)
 
-// determine if the box clicked is equal to the random number
-// if the numbers match, display a winning message by changing the text content of the div#message element
+        // create a random number between 1-3 and store it to a variable
+        // This number will represent the winning box
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+        var randomNum = (getRandomInt(3));
+
+        // determine if the box clicked is equal to the random number
+        // if the numbers match, display a winning message by changing the text content of the div#message element
+        if (numWasClicked === randomNum) {
+            document.getElementById('message').textContent = "You win"
+        } else {
+            document.getElementById('message').textContent = "You lose"
+        }
+      });
+}
+
+
+
+
+
+
 // if the numbers match, increment wins and display the win count in div#wins
 
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
